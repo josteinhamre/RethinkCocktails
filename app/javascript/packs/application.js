689909c832fae524c7773e2398f77005e6511af0
Jostein Hamre <josteinhamre@gmail.com>
2019-09-20 18:32:52 +0200

    Pushing to heroku. Fixet edit partially.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import 'bootstrap';
 import { selectedItems, addForm } from '../components/selectMultiple';
+import { initSelect2 } from '../components/init_select2';
+
 
 // addForm()
-
+// initSelect2()
 selectedItems();
